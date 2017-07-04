63905f8603c424487477169a05c72eab994eb82a
Shashikant86 <shashikant.jagtap@icloud.com>
2017-07-04 22:58:52 +0100

    added example of non scalable test

---
+++ Scalable-XCUITestUITests/Non_Scalable_XCUITestUITests.swift
@@ -0,0 +1,44 @@
+//
+//  Non_Scalable_XCUITestUITests.swift
+//  Scalable-XCUITestUITests
+//
+//  Created by Shashikant Jagtap on 04/07/2017.
+//  Copyright © 2017 Shashikant Jagtap. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class Non_Scalable_XCUITestUITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+    func testTapAllColorNonScalable() {
+        XCUIApplication().buttons["RED"].tap()
+        XCUIApplication().buttons["BLUE"].tap()
+        XCUIApplication().buttons["GREEN"].tap()
+        XCUIApplication().buttons["BLACK"].tap()
+        XCUIApplication().buttons["YELLOW"].tap()
+        XCUIApplication().buttons["GRAY"].tap()
+        
+    }
+    
+    func testRBGBlueButtonNonScalable() {
+        XCUIApplication().buttons["BLUE"].tap()
+    }
+    
+    func testNonRBGYelloButtonNonScalable() {
+        XCUIApplication().buttons["YELLOW"].tap()
+    }
+    
+}
